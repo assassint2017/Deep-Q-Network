933fb2b0354eca29a0f45532bdbf995fc94690ec
Zcy <145958061@qq.com>
2018-07-17 22:16:14 +0800

    add Double_DQN

---
+++ Double_DQN/Double_DQN.py
@@ -0,0 +1,154 @@
+"""
+
+构建 Double DQN
+和最原始的DQN相比，区别在于q_target的计算，
+DDQN的q_target的值，是通过使用evel_net中的最大q值来为target_net选择动作计算出来的，
+而不是直接选择target_net中最大的q值
+"""
+
+import os
+import random
+
+import numpy as np
+import torch
+import torch.nn as nn
+
+from Qnet import eval_net
+from Qnet import target_net
+
+batch_size = 32
+leaing_rate = 1e-2
+
+memory_pool_capacity = 2000  # 经验池的大小
+target_net_update = 100  # 目标网络每多少步更新一次
+
+num_state = 4  # 一个observation由几个数字构成
+num_action = 2  # 有哪几种可能选择的动作
+
+gamma = 0.9  # 未来奖励的折扣因子
+
+# e-greedy选取动作策略
+epsilon_min = 0.5  # 最开始的e值
+epsilon_max = 0.9  # 最大的e值
+
+module_dir = './module/'  # 保存训练好的模型的路径
+
+
+class DQN(object):
+    def __init__(self):
+
+        # 记忆库的容量
+        self.memory_pool_capacity = memory_pool_capacity
+
+        # 构建记忆库
+        self.memory_pool = np.zeros((memory_pool_capacity, num_state * 2 + 3))
+
+        # 用于记录记忆库当前的大小
+        self.memory_pool_size = 0
+
+        # 用于记录网络已经更新了多少步
+        self.update_step = 0
+
+        # 定义优化器和损失函数
+        self.optimizer = torch.optim.Adam(eval_net.parameters(), leaing_rate)
+        self.loss_func = nn.MSELoss()
+
+        # 对于epsilon的数值，在训练初期应该是相对较低，这样有利于进行探索
+        # 随着不断的训练，应该逐渐提升epsilon的数值，因为这时候网络的预测结果正在变得越来越精确，越可以进行贪心选择
+        self.epsilon = epsilon_min
+
+    def choose_action(self, observation):
+        """
+
+        :param observation: 输入状态
+        :return: 按照e-greedy策略进行选择动作
+        """
+        if self.memory_pool_size >= self.memory_pool_capacity:
+            self.epsilon += 2e-5
+
+        if self.epsilon > epsilon_max:
+            self.epsilon = epsilon_max
+
+        if random.uniform(0, 1) < self.epsilon:  # 执行贪心
+            observation = torch.FloatTensor(observation).unsqueeze(dim=0)
+            with torch.no_grad():
+                action_q_vecotr = eval_net(observation)
+            action = int(torch.max(action_q_vecotr, dim=1)[1])
+
+        else:  # 随机选取动作
+            action = random.randint(0, num_action - 1)
+
+        return action
+
+    def store_memory(self, s, a, r, s_, done):
+        """
+
+        :param s: 当前状态 ndarray
+        :param a: 在当前状态下执行的动作 int
+        :param r: 在当前状态下执行动作后得到的奖励 float
+        :param s_: 在当前状态下执行的动作后进行的下一状态 ndarray
+        :param done: 在当前状态下执行动作后有没有导致本次episode结束的标志位
+        """
+        if done is True:
+            done = 0
+        else:
+            done = 1
+
+        memory = np.hstack((s, a, r, s_, done))
+
+        # 用新的记忆来覆盖老的记忆
+        index = self.memory_pool_size % memory_pool_capacity
+
+        self.memory_pool[index, :] = memory
+        self.memory_pool_size += 1
+
+    def update_parameter(self):
+
+        # 每间隔一定的时间才更新一次目标网络
+        if self.update_step % target_net_update is 0:
+            target_net.load_state_dict(eval_net.state_dict())
+
+        # 每次都要对eval网络进行更新
+        # 随机从记忆库中抽取mini batch个数据进行训练网络
+        sample_index = np.random.choice(memory_pool_capacity, batch_size)
+        memory_batch = self.memory_pool[sample_index, :]
+
+        observation_batch = torch.FloatTensor(memory_batch[:, :num_state])
+        action_batch = torch.LongTensor(memory_batch[:, num_state]).unsqueeze(dim=1)
+        reward_batch = torch.FloatTensor(memory_batch[:, num_state + 1]).unsqueeze(dim=1)
+        next_observation_batch = torch.FloatTensor(memory_batch[:, num_state + 2:-1])
+        done_batch = torch.FloatTensor(memory_batch[:, -1]).unsqueeze(dim=1)
+
+        # 估计的q值 q_eval
+        q_eval = eval_net(observation_batch).gather(1, action_batch)
+
+        # 现实中的q值 q_target，这里由于不用对target网络进行更新，因此仅使用farward模型即可
+        # 这里许多乘一个done_batch的判断位信息，因为如果本回合已经结束，这时候的q_target应该应该只包括当前状态的奖励
+        # 而不包括下一状态的最大Q值
+
+        # 这部分就是ddqn相对于原始dqn的改动之处
+        with torch.no_grad():
+
+            action_index = eval_net(next_observation_batch).max(1)[1].unsqueeze(dim=1)
+            q_next = target_net(next_observation_batch).gather(1, action_index)
+
+            q_target = reward_batch + gamma * q_next * done_batch
+        # 根据现实和估计的q值之间的差距计算loss并进行反向传播
+        loss = self.loss_func(q_eval, q_target)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        self.update_step += 1
+
+    def save(self, score):
+        """
+
+        :param score: 根据本轮episode获得的reward
+        :return: 保存模型
+        """
+        torch.save(eval_net.state_dict(), os.path.join(module_dir, 'best_reward_{:.1f}.pth'.format(score)))
+
+
+dqn = DQN()
---
+++ Double_DQN/Qnet.py
@@ -0,0 +1,74 @@
+"""
+
+创建Q网络，这里就使用简单的全连接网络即可
+"""
+
+import torch.nn as nn
+
+num_state = 4
+num_action = 2
+
+
+class QNet(nn.Module):
+    def __init__(self, num_states, num_actions):
+        """
+
+        :param num_states: 输入的状态数量
+        :param num_actions: 输出的动作数量
+        """
+        super().__init__()
+
+        self.fc1 = nn.Sequential(
+            nn.Linear(num_states, 50),
+            nn.ReLU()
+        )
+
+        self.fc2 = nn.Sequential(
+            nn.Linear(50, 50),
+            nn.ReLU()
+        )
+
+        self.fc3 = nn.Sequential(
+            nn.Linear(50, 50),
+            nn.ReLU()
+        )
+
+        self.fc4 = nn.Linear(50, num_actions)
+
+    def forward(self, state):
+        """
+
+        :param state: 输入一个状态
+        :return: 返回每个可选择的动作对应的Q值
+        """
+        outputs = self.fc1(state)
+        outputs = self.fc2(outputs)
+        outputs = self.fc3(outputs)
+        q_vector = self.fc4(outputs)
+
+        return q_vector
+
+
+def weight_initialization(module):
+    if isinstance(module, nn.Linear):
+        nn.init.normal(module.weight.data, std=0.1)
+        nn.init.constant(module.bias.data, 0)
+
+
+eval_net = QNet(num_state, num_action)
+target_net = QNet(num_state, num_action)
+
+eval_net.apply(weight_initialization)
+target_net.apply(weight_initialization)
+
+
+# # 测试GPU CPU速度代码
+# # 发现GPU比CPU稍慢，所以放弃使用GPU
+# import torch
+# from time import time
+#
+# eval_net = eval_net.cuda()
+# start_time = time()
+# data = torch.randn((32, 4)).cuda()
+# res = eval_net(data)
+# print(time() - start_time)
